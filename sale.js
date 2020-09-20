140ddc5d32f1ff5647530256af52df09a44aba94
dohuthyfa <dohuthayfa@gmail.com>
2020-09-20 17:18:20 +0300

    Sale-App

---
+++ sale.js
@@ -0,0 +1,7 @@
+ 
+
+ function change(value){
+          document.getElementById("id="apple amount).totalValue= apple price*apple amount;
+          document.getElementById("totalValue").innerHTML= "Total price: $" + 500*value;
+          }
+          
