5bd318b4cbecf9ab3e1967ba971bb644b16a4ee4
dohuthayfa <65371408+dohuthayfa@users.noreply.github.com>
2020-09-21 13:05:23 +0300

    Update sale.js

--- sale.js
+++ sale.js
@@ -1,7 +1,15 @@
  
 
- function change(value){
-          document.getElementById("id="apple amount).totalValue= apple price*apple amount;
-          document.getElementById("totalValue").innerHTML= "Total price: $" + 500*value;
-          }
-          
+ $("#t1").click(function(){
+            var amount1 = $("#a1").val();
+            console.log(amount1)
+            var amount2 = $("#o1").val();
+            console.log(amount2)
+            var result=amount1*5 + amount2*4;
+           console.log(result)
+           document.getElementById("totalValue").value = result;
+
+
+
+
+        });
